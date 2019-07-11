5bc2d34aab580d0aa8df16bb5b7bffbe48172b09
Ahmed <ahsherif@microsoft.com>
2019-07-11 19:03:33 -0400

    updating views in flask app

---
+++ pypi_org/views/package_views.py
@@ -0,0 +1,10 @@
+import flask 
+import sys
+sys.path.append('pypi_org/services')
+from services import package_services
+
+blueprint = flask.Blueprint('packages', __name__, template_folder = 'templates')
+
+@blueprint.route("/project/<package_name>")
+def package_details(package_name: str):
+    return "Package details for {}".format(package_name)
