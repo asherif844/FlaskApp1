e751b429e3e761187874ae3abd33dc00ff902250
Ahmed <ahsherif@microsoft.com>
2019-07-12 23:13:30 -0400

    day 5 updates

--- pypi_org/views/package_views.py
+++ pypi_org/views/package_views.py
@@ -8,3 +8,8 @@ blueprint = flask.Blueprint('packages', __name__, template_folder = 'templates')
 @blueprint.route("/project/<package_name>")
 def package_details(package_name: str):
     return "Package details for {}".format(package_name)
+
+@blueprint.route("/<int:rank>")
+def popular(rank: int):
+    print(type(rank), rank)
+    return "The details for {}th most popular package".format(rank)
